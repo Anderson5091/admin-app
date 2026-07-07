490eef0aa920e7fb14a21de431ed925e206ff976
Anderson5091 <anderson5091@gmail.com>
2026-07-07 12:46:40 -0400

    fixing build errors

--- src/features/admin/admin.store.ts
+++ src/features/admin/admin.store.ts
@@ -86,6 +86,7 @@ export const useAdminStore = create<AdminState>((set) => ({
   dashboard: null,
   users: [],
   pendingKyc: [],
+  kycDetail: null,
   complianceCases: [],
   failedPayouts: [],
   executedPayouts: [],
